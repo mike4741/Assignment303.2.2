687df655dd4984e02e4531b84794b76252345299
mike <me.menge47@gmail.com>
2022-11-30 17:45:54 -0600

    make change on tax calculator class

--- exercise/TaxCalc.java
+++ exercise/TaxCalc.java
@@ -40,22 +40,22 @@ public class TaxCalc {
 			if (income > 372951) {
 				taxVal = income * 35 / 100;
 
-			} else if (income >= 171551 || income <= 372950) {
+			} else if (income >= 171551 && income <= 372950) {
 				taxVal = income * 33 / 100;
 
-			} else if (income >= 82251 || income <= 171550) {
+			} else if (income >= 82251 && income <= 171550) {
 
 				taxVal = income * 28 / 100;
 
-			} else if (income >= 33951 || income <= 82250) {
+			} else if (income >= 33951 && income <= 82250) {
 
 				taxVal = income * 25 / 100;
 
-			} else if (income >= 8351 || income <= 33950) {
+			} else if (income >= 8351 && income <= 33950) {
 
 				taxVal = income * 15 / 100;
 
-			} else if (income > 0 || income <= 8350) {
+			} else if (income > 0 && income <= 8350) {
 
 				taxVal = income * 10 / 100;
 			}
@@ -65,22 +65,22 @@ public class TaxCalc {
 			if (income > 372951) {
 				taxVal = income * 35 / 100;
 
-			} else if (income >= 208851 || income <= 372950) {
+			} else if (income >= 208851 && income <= 372950) {
 				taxVal = income * 33 / 100;
 
-			} else if (income >= 137051 || income <= 208850) {
+			} else if (income >= 137051 && income <= 208850) {
 
 				taxVal = income * 28 / 100;
 
-			} else if (income >= 67901 || income <= 137050) {
+			} else if (income >= 67901 && income <= 137050) {
 
 				taxVal = income * 25 / 100;
 
-			} else if (income >= 16701 || income <= 67900) {
+			} else if (income >= 16701 && income <= 67900) {
 
 				taxVal = income * 15 / 100;
 
-			} else if (income > 0 || income <= 16700) {
+			} else if (income > 0 && income <= 16700) {
 
 				taxVal = income * 10 / 100;
 			}
@@ -90,22 +90,22 @@ public class TaxCalc {
 			if (income > 186476) {
 				taxVal = income * 35 / 100;
 
-			} else if (income >= 104426 || income <= 186475) {
+			} else if (income >= 104426 && income <= 186475) {
 				taxVal = income * 33 / 100;
 
-			} else if (income >= 68526 || income <= 104425) {
+			} else if (income >= 68526 && income <= 104425) {
 
 				taxVal = income * 28 / 100;
 
-			} else if (income >= 33951 || income <= 68525) {
+			} else if (income >= 33951 && income <= 68525) {
 
 				taxVal = income * 25 / 100;
 
-			} else if (income >= 8351 || income <= 33950) {
+			} else if (income >= 8351 && income <= 33950) {
 
 				taxVal = income * 15 / 100;
 
-			} else if (income > 0 || income <= 8350) {
+			} else if (income > 0 && income <= 8350) {
 
 				taxVal = income * 10 / 100;
 			}
@@ -116,22 +116,22 @@ public class TaxCalc {
 			if (income > 372951) {
 				taxVal = income * 35 / 100;
 
-			} else if (income >= 190201 || income <= 372950) {
+			} else if (income >= 190201 && income <= 372950) {
 				taxVal = income * 33 / 100;
 
-			} else if (income >= 117451 || income <= 190200) {
+			} else if (income >= 117451  && income <= 190200) {
 
 				taxVal = income * 28 / 100;
 
-			} else if (income >= 45501 || income <= 117450) {
+			} else if (income >= 45501 &&  income <= 117450) {
 
 				taxVal = income * 25 / 100;
 
-			} else if (income >= 11950 || income <= 45500) {
+			} else if (income >= 11950 && income <= 45500) {
 
 				taxVal = income * 15 / 100;
 
-			} else if (income > 0 || income <= 11950) {
+			} else if (income > 0 && income <= 11950) {
 
 				taxVal = income * 10 / 100;
 			}
